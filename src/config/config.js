62b7f9b36b6ac8fa018492a781f498811b351bc2
liuweigl <wei2460@qq.com>
2020-12-30 07:14:30 +0800

    chore: clean workspace

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  componentName: 'VuePageStack',
-  keyName: 'stack-key',
-  pushName: 'push',
-  goName: 'go',
-  replaceName: 'replace',
-  backName: 'back',
-  forwardName: 'forward'
-};
