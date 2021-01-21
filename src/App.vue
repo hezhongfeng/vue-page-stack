401baa21a5bc0f70232cffd518d820e4c2fe8b38
hezhongfeng <287645587@qq.com>
2021-01-21 10:33:35 +0800

    feat(vant): 添加vant UI库

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,8 @@ import HelloWorld from './components/HelloWorld.vue';
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 });
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+createApp(App).use(Vant).mount('#app');
