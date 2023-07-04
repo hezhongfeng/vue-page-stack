16fdeb56856b0709c5c322771eaf916fff598bde
hezhongfeng <287645587@qq.com>
2023-07-04 16:36:05 +0800

    :bug: fix: 添加钩子函数

--- dist/vue-page-stack.es.js
+++ dist/vue-page-stack.es.js
@@ -1,5 +1,5 @@
-import { defineComponent as D, getCurrentInstance as S, queuePostFlushCb as V, onMounted as b, onUpdated as K, onBeforeUnmount as g, isVNode as v, cloneVNode as w, setTransitionHooks as q, callWithAsyncErrorHandling as B } from "vue";
-import { useRoute as W } from "vue-router";
+import { defineComponent as S, getCurrentInstance as b, queuePostFlushCb as R, onMounted as K, onUpdated as g, onBeforeUnmount as v, isVNode as w, cloneVNode as q, setTransitionHooks as B, callWithAsyncErrorHandling as W } from "vue";
+import { useRoute as G } from "vue-router";
 const E = {
   componentName: "VuePageStack",
   keyName: "stack-key",
@@ -13,18 +13,18 @@ var p;
 (function(e) {
   e[e.ELEMENT = 1] = "ELEMENT", e[e.FUNCTIONAL_COMPONENT = 2] = "FUNCTIONAL_COMPONENT", e[e.STATEFUL_COMPONENT = 4] = "STATEFUL_COMPONENT", e[e.TEXT_CHILDREN = 8] = "TEXT_CHILDREN", e[e.ARRAY_CHILDREN = 16] = "ARRAY_CHILDREN", e[e.SLOTS_CHILDREN = 32] = "SLOTS_CHILDREN", e[e.TELEPORT = 64] = "TELEPORT", e[e.SUSPENSE = 128] = "SUSPENSE", e[e.COMPONENT_SHOULD_KEEP_ALIVE = 256] = "COMPONENT_SHOULD_KEEP_ALIVE", e[e.COMPONENT_KEPT_ALIVE = 512] = "COMPONENT_KEPT_ALIVE", e[e.COMPONENT = 6] = "COMPONENT";
 })(p || (p = {}));
-const h = (e, n) => {
+const V = (e, n) => {
   for (let r = 0; r < e.length; r++)
     e[r](n);
 };
-function I(e, n, r, o) {
-  B(e, n, X.VNODE_HOOK, [r, o]);
+function h(e, n, r, o) {
+  W(e, n, Y.VNODE_HOOK, [r, o]);
 }
-const G = (e) => e.__isSuspense, H = {
+const X = (e) => e.__isSuspense, I = {
   ENTER: 0,
   LEAVE: 1,
   REORDER: 2
-}, X = {
+}, Y = {
   SETUP_FUNCTION: 0,
   RENDER_FUNCTION: 1,
   WATCH_GETTER: 2,
@@ -34,7 +34,7 @@ const G = (e) => e.__isSuspense, H = {
   COMPONENT_EVENT_HANDLER: 6,
   VNODE_HOOK: 7
 };
-function Y(e) {
+function H(e) {
   e.shapeFlag &= ~p.COMPONENT_SHOULD_KEEP_ALIVE, e.shapeFlag &= ~p.COMPONENT_KEPT_ALIVE;
 }
 function P(e) {
@@ -45,11 +45,11 @@ const N = [], U = (e) => {
     if (N[n].key === e)
       return n;
   return -1;
-}, j = (e) => D({
+}, j = (e) => S({
   name: E.componentName,
   __isKeepAlive: !0,
   setup(n, { slots: r }) {
-    const o = S(), c = o.ctx, u = o.suspense, {
+    const o = b(), c = o.ctx, u = o.suspense, {
       renderer: {
         p: T,
         m: L,
@@ -59,50 +59,50 @@ const N = [], U = (e) => {
     } = c, y = k("div");
     c.activate = (t, O, l, a, s) => {
       const i = t.component;
-      L(t, O, l, H.ENTER, u), T(i.vnode, t, O, l, i, u, a, t.slotScopeIds, s), V(() => {
-        i.isDeactivated = !1, i.a && h(i.a);
+      L(t, O, l, I.ENTER, u), T(i.vnode, t, O, l, i, u, a, t.slotScopeIds, s), R(() => {
+        i.isDeactivated = !1, i.a && V(i.a);
         const _ = t.props && t.props.onVnodeMounted;
-        _ && I(_, i.parent, t);
+        _ && h(_, i.parent, t);
       }, u);
     }, c.deactivate = (t) => {
       const O = t.component;
-      L(t, y, null, H.LEAVE, u), V(() => {
-        O.da && h(O.da);
+      L(t, y, null, I.LEAVE, u), R(() => {
+        O.da && V(O.da);
         const l = t.props && t.props.onVnodeUnmounted;
-        l && I(l, O.parent, t), O.isDeactivated = !0;
+        l && h(l, O.parent, t), O.isDeactivated = !0;
       }, u);
     };
-    function A(t) {
-      Y(t), x(t, o, u, !0);
+    function D(t) {
+      H(t), x(t, o, u, !0);
     }
     let f = null, C = !1;
-    const R = () => {
+    const A = () => {
       f != null && (C ? N[N.length - 1].vnode = P(o.subTree) : N.push({ key: f, vnode: P(o.subTree) }));
     };
-    return b(R), K(R), g(() => {
+    return K(A), g(A), v(() => {
       for (const t of N)
-        A(t.vnode);
+        D(t.vnode);
     }), () => {
       f = null, C = !1;
-      const O = W().query[e];
+      const O = G().query[e];
       if (!r.default)
         return null;
       const l = r.default(), a = l[0];
       if (l.length > 1)
         return l;
-      if (!v(a) || !(a.shapeFlag & p.STATEFUL_COMPONENT) && !(a.shapeFlag & p.SUSPENSE))
+      if (!w(a) || !(a.shapeFlag & p.STATEFUL_COMPONENT) && !(a.shapeFlag & p.SUSPENSE))
         return a;
       let s = P(a);
-      s.el && (s = w(s), a.shapeFlag & p.SUSPENSE && (a.ssContent = s)), f = O;
+      s.el && (s = q(s), a.shapeFlag & p.SUSPENSE && (a.ssContent = s)), f = O;
       let i = U(O);
       if (i !== -1) {
         const _ = N[i].vnode;
-        s.el = _.el, s.component = _.component, s.transition && q(s, s.transition), s.shapeFlag |= p.COMPONENT_KEPT_ALIVE;
+        s.el = _.el, s.component = _.component, s.transition && B(s, s.transition), s.shapeFlag |= p.COMPONENT_KEPT_ALIVE;
         for (let m = i + 1; m < N.length; m++)
-          A(N[m].vnode), N[m] = null;
+          H(N[m].vnode), N[m] = null;
         N.splice(i + 1), C = !0;
       }
-      return s.shapeFlag |= p.COMPONENT_SHOULD_KEEP_ALIVE, G(a.type) ? a : s;
+      return s.shapeFlag |= p.COMPONENT_SHOULD_KEEP_ALIVE, X(a.type) ? a : s;
     };
   }
 }), d = {
